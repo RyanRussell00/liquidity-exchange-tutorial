181349a85c8b9a3328df161529ccb74bae4c8bfb
Ryan Russell <ryan.russell2000@gmail.com>
2021-08-14 13:45:53 -0700

    Completed tutorial

--- packages/buidler/scripts/deploy.js
+++ packages/buidler/scripts/deploy.js
@@ -15,13 +15,13 @@ async function main() {
   const dex = await deploy("DEX",[balloons.address])
 
   // paste in your address here to get 10 balloons on deploy:
-  await balloons.transfer("0x2d0B23210A6E04727842fD341Aefd5318C8eBC70",""+(10*10**18))
+  await balloons.transfer("0x99a1d76EfB95ED39f922AAE8D8ca62eD355ee8c4",""+(10*10**18))
 
   // uncomment to init DEX on deploy:
-  //console.log("Approving DEX ("+dex.address+") to take Balloons from main account...")
-  //await balloons.approve(dex.address,ethers.utils.parseEther('100'))
-  //console.log("INIT exchange...")
-  //await dex.init(ethers.utils.parseEther('5'),{value:ethers.utils.parseEther('5')})
+  console.log("Approving DEX ("+dex.address+") to take Balloons from main account...")
+  await balloons.approve(dex.address,ethers.utils.parseEther('100'))
+  console.log("INIT exchange...")
+  await dex.init(ethers.utils.parseEther('5'),{value:ethers.utils.parseEther('5')})
 
 }
 main()
